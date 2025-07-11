c9408fc937fae8b4902ad47e33de873ee9baf216
HomGraziano <84432114+HomGraziano@users.noreply.github.com>
2025-07-11 19:17:31 -0300

    Add files via upload

--- inventory_app/models.py
+++ inventory_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100,verbose_name="Producto:")
-    quantity = models.IntegerField(default=0,verbose_name="Precio Unitario:")
-    price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Cantidad:")
+    quantity = models.IntegerField(default=0,verbose_name="Cantidad:")
+    price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Precio:")
 
     def __str__(self):
         return f"{self.name} ({self.quantity})"
